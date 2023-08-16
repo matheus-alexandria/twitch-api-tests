507b72b65a931e2a464b9e25c1255444eabe28e4
Matchobas <maalexandria@hotmail.com>
2023-08-15 22:50:49 -0300

    feat: testing connection with twitch in fastify

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,20 @@
 import fastify from 'fastify';
+import { env } from './env';
 
 const app = fastify();
+
+app.get('/', async (request, reply) => {
+  const data = await fetch('https://api.twitch.tv/helix/users?login=cellbit', {
+    headers: {
+      'Authorization': `Bearer ${env.AUTH_TOKEN}`,
+      'Client-Id': env.CLIENT_ID
+    }
+  });
+
+  console.log(data);
+
+  return reply.send();
+});
 
 app.listen({
   port: 3333,
